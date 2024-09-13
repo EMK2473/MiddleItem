6ef488b9496a401a9a2343416b8513a48ccfc290
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 16:27:14 -0400

    init file

---
+++ MiddleItem.java
@@ -0,0 +1,12 @@
+package MiddleItem;
+
+import java.util.Scanner; 
+
+public class MiddleItem {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int[] userValues = new int[9];  // Set of data specified by the user      
+
+      /* Type your code here. */
+   }
+}
