82af586f6a02768c01e1c96300e1b761ea939bf7
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 17:33:21 -0400

    declared and assigned a count variable to track positive ints; for looped through input assigning positive ints to arr index; incrementing count for each positive int; prints for visual/testing

--- MiddleItem.java
+++ MiddleItem.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 public class MiddleItem {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int[] userValues = new int[9];  // Set of data specified by the user      
+      int[] userValues = new int[9]; 
+      int count = 0;    
 
-      /* Type your code here. */
+      for(int i = 0; i < userValues.length; ++i) {
+        userValues[i] = scnr.nextInt();
+
+        if(userValues[i] < 0){
+            System.out.println(userValues[i] + " is not positive");
+            break;
+        }
+        count++;
+        System.out.println("count: " + count);
+
+      }
    }
 }
